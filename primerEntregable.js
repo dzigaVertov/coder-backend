c31c08aaeeec62a05836cf9266d61bf8082e7180
Marcelo Gómez <marcelo@pop-os.localdomain>
2023-02-16 12:04:23 -0300

    initial commit

---
+++ primerEntregable.js
@@ -0,0 +1,18 @@
+class ProductManager {
+    constructor(){
+        this.products = [];
+    }
+    
+}
+
+class Product {
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+    
+}
