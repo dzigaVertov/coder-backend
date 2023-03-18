cf6f50d7dd64561ef705996d6623d34d0620d2a4
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-03-18 18:41:55 -0300

    agregado handlebars

--- src/servidor.js
+++ src/servidor.js
@@ -1,10 +1,32 @@
 import express from 'express';
-
+import { engine } from 'express-handlebars';
+import { Server as SocketIOServer } from 'socket.io';
 import apiProductsRouter from './Routers/apiProductsRouter.js';
 import apiCartsRouter from './Routers/apiCartsRouter.js';
+import ProductManager from './ProductManager.js';
+
+
 const app = express();
+app.engine('handlebars', engine());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
+
+app.use(express.static('./public'));
+
+const httpServer = app.listen(8080, () => console.log('listening on port 8080'));
+const io = new SocketIOServer(httpServer);
+
+io.on('connection', async clientSocket => {
+    console.log(`Nuevo cliente conectado: socket id: ${clientSocket.id}`) ;
+});
 
 app.use('/api/products', apiProductsRouter);
 app.use('/api/carts', apiCartsRouter);
 
-const server = app.listen(8080, () => console.log('listening on port 8080'));
+const managerProductos = new ProductManager('./src/products.json');
+app.get('/', async (req, res) => {
+    let productos = await managerProductos.getProducts();
+    res.render('home', { pageTitle: 'éxito', productos: productos });
+});
+
+
