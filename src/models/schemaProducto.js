da5dbc5bd694490e67eb6bebc3aa16977f43cac1
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-04-07 18:41:19 -0300

    agregada validación al Schema

--- src/models/schemaProducto.js
+++ src/models/schemaProducto.js
@@ -2,18 +2,40 @@ import mongoose from 'mongoose';
 
 const schemaProducto = new mongoose.Schema(
     {
-        title: String,
-        description: String,
-        price: Number,
+        title: {
+            type: String,
+            required: [true, "El campo es requerido"]
+        },
+        description: {
+            type: String,
+            required: [true, "El campo es requerido"]
+        },
+        price: {
+            type: Number,
+            required: [true, "El campo es requerido"]
+        },
         thumbnail: String,
         code: {
             type: String,
             unique: true,
-            dropDups: true
-            },
-        stock: Number,
-        category: String,
-        status: String,
+            dropDups: true,
+            required: {
+                type: String,
+                required: [true, "El campo es requerido"]
+            }
+        },
+        stock: {
+            type: Number,
+            required: [true, "El campo es requerido"]
+        },
+        category: {
+            type: String,
+            required: [true, "El campo es requerido"]
+        },
+        status: {
+            type: String,
+            required: [true, "El campo es requerido"]
+        },
     }
 );
 
