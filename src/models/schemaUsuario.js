0b57b3090eda9cd73e7ca9862aeb0082fc19bd87
Marcelo Gómez <marcelo@pop-os.localdomain>
2023-05-06 23:21:23 -0300

    cambio pc 06-05

--- src/models/schemaUsuario.js
+++ src/models/schemaUsuario.js
@@ -6,6 +6,12 @@ const schemaUsuario = new mongoose.Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     age: { type: Number, required: true },
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'cartsCollection'
+    },
+    role: {type: String, required: true, default:'user'}
+
 }, { versionKey: false });
 
 export const usuarioModel = mongoose.model('usuarios', schemaUsuario);
