a011d930ff3ffdce0ee624b707f4e6fc52accb64
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-06-12 21:33:59 -0300

    borrado archivo remanente

--- src/Routers/apiJwtRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { usuarioModel } from '../models/schemaUsuario.js';
-
-export const apiJwtRouter = Router();
-
-
-apiJwtRouter.post('/login', async (req, res)=> {
-    
-})
