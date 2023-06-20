3b6ea40dd11173fd1ed42046ca0b6cd621520467
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-06-19 21:17:40 -0300

    agregado logger winston

---
+++ src/utils/logger.js
@@ -0,0 +1,49 @@
+import winston from 'winston';
+import { NODE_ENV } from '../config/servidor.config.js';
+
+const levels = {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    http: 4,
+    debug: 5
+};
+
+const winstonLoggerDev = winston.createLogger({
+    levels,
+    transports: [
+        new winston.transports.Console({
+            level: 'debug'
+        }),
+        new winston.transports.File({
+            level: 'error',
+            filename: 'errors.log'
+        })
+    ]
+});
+
+const winstonLoggerProd = winston.createLogger({
+    levels,
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: 'info.log'
+        }),
+        new winston.transports.File({
+            level: 'error',
+            filename: 'errors.log'
+        })
+    ]
+});
+
+export let logger;
+if (NODE_ENV === 'production') {
+    logger = winstonLoggerProd;
+} else {
+    logger = winstonLoggerDev;
+}
+
+
+
+
--- src/utils/paginacion.js
+++ src/utils/paginacion.js
@@ -1,8 +1,8 @@
 export function getLinksPaginacion(queryResult,baseUrl, category = 'all', sort = 'none', sortField = 'price') {
     let { hasPrevPage, hasNextPage, prevPage, nextPage, limit } = queryResult;
 
-    let linkPrevPage = hasPrevPage ? (baseUrl + `?paginate=true&limit=${limit}&page=${prevPage}&sort=${sort}&sortField=${sortField}`) : null;
-    let linkNextPage = hasNextPage ? (baseUrl + `?paginate=true&limit=${limit}&page=${nextPage}&sort=${sort}&sortField=${sortField}`) : null;
+    let linkPrevPage = hasPrevPage ? (baseUrl + `?paginate=true&limit=${limit}&page=${prevPage}&category=${category}&sort=${sort}&sortField=${sortField}`) : null;
+    let linkNextPage = hasNextPage ? (baseUrl + `?paginate=true&limit=${limit}&page=${nextPage}&category=${category}&sort=${sort}&sortField=${sortField}`) : null;
 
     return [linkPrevPage, linkNextPage];
 }
