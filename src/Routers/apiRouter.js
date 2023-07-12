fdbd42e91f246f4a8ddfe9d2af9a809bf138279c
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-07-11 23:52:15 -0300

    agregada documentación con swagger

--- src/Routers/apiRouter.js
+++ src/Routers/apiRouter.js
@@ -5,14 +5,18 @@ import apiProductsRouter from './apiProductsRouter.js';
 import apiUsersRouter from './apiUsersRouter.js';
 import { mockingProductsRouter } from './mockingProductsRouter.js';
 import { apiErrorHandler } from '../middlewares/apiErrorHandler.js';
+import { docsRouter } from './documentacionRouter.js';
 
 const apiRouter = Router();
+
+apiRouter.use('/docs', docsRouter);
 
 apiRouter.use('/sessions', apiSessionsRouter);
 apiRouter.use('/users', apiUsersRouter);
 apiRouter.use('/products', apiProductsRouter);
 apiRouter.use('/carts', apiCartsRouter);
 apiRouter.use('/mockingProducts', mockingProductsRouter);
+
 apiRouter.use(apiErrorHandler);
 
 export default apiRouter;
